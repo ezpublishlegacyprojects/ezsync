00884dd0485dca2d92550b5ff6d44f642501f598
cphp <info@example.com>
2008-08-25 14:28:03 +0000

    add readme file
    git-svn-id: http://svn.projects.ez.no/ezsync@6 09b8f0dc-31da-4b58-a9ad-5375990ff8cf

--- trunk/settings/sync.ini.append.php
+++ trunk/settings/sync.ini.append.php
@@ -1,18 +1,15 @@
 <?php /*
 
 [DefaultSyncSettings]
-Host=www.eharmony.it
-Dir=/var/www/harmony_staging
-User=eharmony
-Port=2222
+Host=
+Dir=
+User=
+Port=22
 Parameters=
 FileRsyncEclude=extension/ezsync/settings/rsync_exclude.txt
 
-[StagingSyncSettings]
-Dir=/var/www/harmony_staging
-
 [ProductionSyncSettings]
-Dir=/var/www/harmony
+Dir=
 
 
 */ ?>
