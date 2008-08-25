93dfbd0389e2af86a7fa412dc23873ab26ebb606
cphp <info@example.com>
2008-08-25 14:08:56 +0000

    Update licence info
    git-svn-id: http://svn.projects.ez.no/ezsync@5 09b8f0dc-31da-4b58-a9ad-5375990ff8cf

--- trunk/modules/courtesy/module.php
+++ trunk/modules/courtesy/module.php
@@ -1,26 +1,27 @@
 <?php
-// SOFTWARE NAME: eZ Flow
-// SOFTWARE RELEASE: 1.0.0
-// COPYRIGHT NOTICE: Copyright (C) 1999-2007 eZ Systems AS
-// SOFTWARE LICENSE: GNU General Public License v2.0
+// SOFTWARE NAME: eZ Deploy
+// SOFTWARE RELEASE: 0.1
+// COPYRIGHT NOTICE: Copyright (C) 2008 idaeto srl
+// SOFTWARE LICENSE: GNU General Public License v3.0
+// AUTHOR: Francesco (cphp) Trucchia - ft@ideato.it
 // NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 //
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //
 // ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
 //
+
 
 $Module = array( 'name' => 'courtesy' );
 
