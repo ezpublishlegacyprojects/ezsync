c97a75eaf72380e7847d893103a197af216f4a88
cphp <info@example.com>
2008-10-31 18:48:34 +0000

    Alcune modifiche per aggiungere il  nome al file e alcuni controlli in più
    git-svn-id: http://svn.projects.ez.no/ezsync@11 09b8f0dc-31da-4b58-a9ad-5375990ff8cf

--- trunk/bin/shell/exportDiffFile.sh
+++ trunk/bin/shell/exportDiffFile.sh
@@ -4,7 +4,14 @@
 #
 # NOTE: remember to commit before lauching the script to catch the latest changes!
 #
-if [ $# -eq 3 ]; then
+
+NAME="diff"
+
+if [ $3 ]; then
+  NAME=$3
+fi
+
+if [ $# -eq 4 ]; then
   function svn {
 	cat listFileDiff.txt
   }
@@ -16,24 +23,36 @@ if [ $# -lt 2 ]; then
   exit
 fi;
 
-svn diff -r $1:$2 --summarize >diff.file
-cat diff.file|head -n -1|grep -v "^D"|awk '{ print $2 }' >diff.ok.file
+svn diff -r $1:$2 --summarize >$NAME.file
+cat $NAME.file|head -n -1|grep -v "^D"|awk '{ print $2 }' >$NAME.ok.file
 
-for f in $( cat  diff.ok.file ); do
+FILESIZE=$(stat -c%s "$NAME.ok.file")
+
+if [ $FILESIZE -eq 0 ]; then
+  echo "Nessuna modifica nel repository"
+  rm $NAME.file
+  rm $NAME.ok.file
+  exit
+fi
+
+for f in $( cat  $NAME.ok.file ); do
     if [ ! -d $f ]; then
-	echo $f >>diff.ok2.file
+	echo $f >>$NAME.ok2.file
     fi
 done
 
-tar czf diff.tar.gz -T diff.ok2.file
-rm diff.ok.file
-rm diff.ok2.file
+tar cf $NAME.tar -T $NAME.ok2.file
+rm $NAME.ok.file
+rm $NAME.ok2.file
 
-echo "Archivio delle differenze 'diff.tar.gz' creato"
+echo "Archivio delle differenze '$NAME.tar' creato"
 # cerca i file da cancellare
 echo "File da cancellare"
-cat  diff.file|grep "^D"
-rm diff.file
+cat  $NAME.file|grep "^D" > delete_diff.txt
+tar rf $NAME.tar delete_diff.txt
+gzip $NAME.tar
+rm $NAME.file
+rm delete_diff.txt
 echo "Fine"
 
 
